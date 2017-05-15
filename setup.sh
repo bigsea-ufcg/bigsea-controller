f60a1143894fce7d75a41ab17a567f47ac86d062
armstrongmsg <armstrongmsg@gmail.com>
2017-05-15 11:07:52 -0300

    Updated requirements

--- setup.sh
+++ setup.sh
@@ -4,5 +4,6 @@ apt-get -y install python-dev
 apt-get -y install python-pip
 apt-get -y install libssl-dev
 apt-get -y install libffi-dev
+python -m pip install appdirs
 pip install -r requirements.txt
 pip install rfc3986
