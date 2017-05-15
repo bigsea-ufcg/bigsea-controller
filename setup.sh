9cf91671a4d99bc7add58c354a8a087a3c28cd3a
armstrongmsg <armstrongmsg@gmail.com>
2017-05-15 10:57:11 -0300

    Updated the requirements to run the scaler

--- setup.sh
+++ setup.sh
@@ -2,5 +2,7 @@ apt-get update
 
 apt-get -y install python-dev
 apt-get -y install python-pip
+apt-get -y install libssl-dev
+apt-get -y install libffi-dev
 pip install -r requirements.txt
 pip install rfc3986
