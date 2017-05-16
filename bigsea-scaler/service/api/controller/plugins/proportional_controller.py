0fcbb05c91df190f101147b9542dcf0cffdcb00a
armstrongmsg <armstrongmsg@gmail.com>
2017-05-16 17:00:16 -0300

    Proportional controller first version

---
+++ bigsea-scaler/service/api/controller/plugins/proportional_controller.py
@@ -0,0 +1,9 @@
+from service.api.controller.controller import Controller
+
+class Proportional_Controller(Controller):
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
