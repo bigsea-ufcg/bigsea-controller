a405362aa64d644eb6a8a2440a00a3508d649c87
Igor Natanael <igor.ataide@ccc.ufcg.edu.br>
2017-07-27 12:28:36 +0200

    Firt commit with actuator for UPV kvm env

--- bigsea-scaler/service/api/actuator/actuator_builder.py
+++ bigsea-scaler/service/api/actuator/actuator_builder.py
@@ -1,4 +1,5 @@
 import ConfigParser
+import paramiko
 
 #from service.api.actuator.instance_locator import Instance_Locator
 from service.api.actuator.plugins.instance_locator_tunnel import Instance_Locator_Tunnel
@@ -11,6 +12,7 @@ from service.api.actuator.plugins.remote_kvm import Remote_KVM
 from service.api.actuator.plugins.nop_actuator import Nop_Actuator
 from service.api.actuator.plugins.service_actuator import Service_Actuator
 from service.api.actuator.plugins.service_instance_locator import Service_Instance_Locator
+from service.api.actuator.plugins.kvm_up import KVM_Actuator_UPV
 
 
 # TODO: documentation
@@ -41,6 +43,15 @@ class Actuator_Builder:
             instance_locator = Instance_Locator_Tunnel(SSH_Utils(hosts_ports), compute_nodes, compute_nodes_keypair)
             remote_kvm = Remote_KVM_Tunnel(SSH_Utils(hosts_ports), compute_nodes_keypair)
             return KVM_Actuator(instance_locator, remote_kvm)
+        elif name == "kvm-upv":
+            conn = paramiko.SSHClient()
+            conn.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            conn.connect(hostname=config.get("actuator", "access_ip"),
+                         username=config.get("actuator", "access_username"),
+                         password=config.get("actuator", "access_password"))
+
+            conn.exec_command("ssh -i hostkey root@niebla.i3m.upv.edu")
+            return KVM_Actuator_UPV(conn)
         elif name == "nop":
             return Nop_Actuator()
         elif name == "service":
---
+++ bigsea-scaler/service/api/actuator/plugins/kvm_upv.py
@@ -0,0 +1,89 @@
+from service.api.actuator.actuator import Actuator
+
+import ConfigParser
+import subprocess
+
+# TODO: documentation
+
+
+class KVM_Actuator_UPV(Actuator):
+
+    def __init__(self, conn):
+        self.conn = conn
+        self.config = ConfigParser.RawConfigParser()
+        self.config.read("scaler.cfg")
+        self.one_user = self.config.get("actuator", "one_username")
+        self.one_password = self.config.get("actuator", "one_password")
+        self.one_url = self.config.get("actuator", "one_url")
+
+
+    # TODO: validation
+    def prepare_environment(self, vm_data):
+        self.adjust_resources(vm_data)
+
+
+    # TODO: validation
+    # This method receives as argument a map {vm-id:CPU cap}
+    def adjust_resources(self, vm_data):
+        instances_locations = {}
+
+        # Discover vm_id - compute nodes map
+        for instance in vm_data.keys():
+            # Access compute nodes to discover vms location
+            instances_locations[instance] = self._find_host(instance)
+
+        for instance in vm_data.keys():
+            # Access a compute node and change cap
+            self._change_vcpu_quota(instances_locations[instance],
+                                    instance, int(vm_data[instance]))
+
+    # TODO: validation
+    def get_allocated_resources(self, vm_id):
+        # Access compute nodes to discover vm location
+        host = self._find_host(vm_id)
+        # ssh for the actual host
+        self.conn.exec_command("ssh %s") % host
+        # List all the vms to get the ONE id and map with the KVM id
+        stdin, stdout, stderr = self.conn.exec_command("virsh list")
+        vm_list = stdout.read().split("\n")
+        virsh_id = self._extract_id(vm_list, vm_id)
+        command = ("virsh schedinfo %s | grep vcpu_quota " +
+                   "| awk '{print $3}'" % (virsh_id))
+        stdin, stdout, stderr = self.conn.exec_command(command)
+        return stdout.read()
+
+    def _change_vcpu_quota(self, host, vm_id, cap):
+        # ssh for the actual host
+        self.conn.exec_command("ssh %s") % host
+        # List all the vms to get the ONE id and map with the KVM id
+        stdin, stdout, stderr = self.conn.exec_command("virsh list")
+        vm_list = stdout.read().split("\n")
+        virsh_id = self._extract_id(vm_list, vm_id)
+        # Set the CPU cap
+        self.conn.exec_command("virsh schedinfo %s " +
+                               "--set vcpu_quota=%s " +
+                               "> /dev/null") % (virsh_id, cap)
+        # Go back to the access node
+        self.conn.exec_command("logout")
+
+    def _find_host(self, vm_id):
+        bashCommand = ("onevm show %s --user %s " +
+                       "--password %s --endpoint %s") % (vm_id, self.one_user,
+                                                         self.one_password, self.one_url)
+        process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+        output, error = process.communicate()
+
+        for line in output.split('\n'):
+            if "HOST" in line and "niebla" in line:
+                return line.split()[2]
+
+        return None
+
+    def _extract_id(self, vm_list, vm_id):
+        one_id = "one-" + vm_id
+        for vm in vm_list:
+            if one_id in vm:
+                return vm.split()[0]
+
+        return None
+
