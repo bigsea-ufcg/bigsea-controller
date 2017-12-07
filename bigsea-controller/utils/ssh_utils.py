85d801571e56e6b0545cbae43358b45d060d202e
armstrongmsg <armstrongmsg@gmail.com>
2017-12-07 12:13:05 -0300

    SSH_Utils now closes the connections after use

--- bigsea-controller/utils/ssh_utils.py
+++ bigsea-controller/utils/ssh_utils.py
@@ -15,6 +15,7 @@
 
 import paramiko
 
+
 class SSH_Utils(object):
 
     def __init__(self, hosts_ports={}):
@@ -23,20 +24,26 @@ class SSH_Utils(object):
     def run_command(self, command, user, host, key):
         conn = self._get_ssh_connection(host, user, key)
         conn.exec_command(command)
- 
+        conn.close()
+
     def run_and_get_result(self, command, user, host, key):
         conn = self._get_ssh_connection(host, user, key)
         stdout = conn.exec_command(command)[1]
-        return stdout.read()
-    
+        result = stdout.read()
+        conn.close()
+        return result
+
     def run_command_tunnel(self, command, user, host, key):
         conn = self._get_ssh_connection_tunnel(host, "root", key)
         conn.exec_command(command)
-        
+        conn.close()
+
     def run_and_get_result_tunnel(self, command, user, host, key):
         conn = self._get_ssh_connection_tunnel(host, "root", key)
         stdout = conn.exec_command(command)[1]
-        return stdout.read()
+        result = stdout.read()
+        conn.close()
+        return result
 
     def _get_ssh_connection(self, ip, username, keypair_path):
         keypair = paramiko.RSAKey.from_private_key_file(keypair_path)
@@ -44,7 +51,7 @@ class SSH_Utils(object):
         conn.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         conn.connect(hostname=ip, username=username, pkey=keypair)
         return conn
-    
+
     def _get_ssh_connection_tunnel(self, ip, username, keypair_path):
         keypair = paramiko.RSAKey.from_private_key_file(keypair_path)
         conn = paramiko.SSHClient()
