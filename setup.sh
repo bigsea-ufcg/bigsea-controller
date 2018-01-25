000f120cae7cf9df5b8e5836d4c22304cf9329a8
armstrongmsg <armstrongmsg@gmail.com>
2018-01-25 16:31:30 -0300

    Added tzlocal to requirements and setuptools is now installed via pip

--- setup.sh
+++ setup.sh
@@ -20,5 +20,6 @@ apt-get -y install python-pip
 apt-get -y install libssl-dev
 apt-get -y install libffi-dev
 python -m pip install appdirs
+pip install setuptools
 pip install -r requirements.txt
 pip install rfc3986
