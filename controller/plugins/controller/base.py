1fb778c5cdb1ddee2c34711e12b72643826bba96
Roberto Nascimento Jr <robertonscjr@gmail.com>
2018-05-03 13:16:35 -0300

    Update plugins structure, fix redundancies on actuator builder and move remotes and locators to 'utils'

---
+++ controller/plugins/controller/base.py
@@ -0,0 +1,84 @@
+# Copyright (c) 2017 LSD - UFCG.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import ConfigParser
+
+from service.api.controller.plugins.basic.controller import Basic_Controller
+from service.api.controller.metric_source_builder import Metric_Source_Builder
+from service.api.actuator.actuator_builder import Actuator_Builder
+from service.api.controller.plugins.single_application_controller import Single_Application_Controller
+from service.api.controller.plugins.generic.controller import Generic_Controller
+from service.api.controller.plugins.tendency.controller import (
+    Tendency_Aware_Proportional_Controller
+)
+from service.api.controller.plugins.proportional.controller import Proportional_Controller
+from service.api.controller.plugins.proportional_derivative.controller import (
+    ProportionalDerivativeController
+)
+from service.api.controller.plugins.pid.controller import PIDController
+from abc import ABCMeta
+from abc import abstractmethod
+from service.api.controller.controller_builder import Controller_Builder
+import threading
+from utils.logger import Log, configure_logging
+
+
+'''
+The Controller is the component responsible for, based on metrics of infrastructure and application
+health such as application progress and CPU usage, taking actions to ensure compliance with
+quality of service levels.
+'''
+
+
+class Controller:
+    __metaclass__ = ABCMeta
+
+    '''
+        Creates a Controller instance.
+
+        app_id is the id of the application associated with the instance.
+        parameters is a dictionary of scaling parameters.
+    '''
+    @abstractmethod
+    def __init__(self, app_id, parameters):
+        pass
+
+    '''
+        Starts scaling for the application associated
+        with the controller instance. The method is not expected
+        to return until the scaling is stopped through the
+        stop_application_scaling. Normally, this method is used
+        as a run method by a thread.
+    '''
+    @abstractmethod
+    def start_application_scaling(self):
+        pass
+
+    '''
+        Stops scaling for the application associated
+        with the controller instance. This method's expected
+        side effect is to make start_application_scaling to return.
+    '''
+    @abstractmethod
+    def stop_application_scaling(self):
+        pass
+
+    '''
+        Returns information on the status of the scaling of applications,
+        normally as a string.
+    '''
+    @abstractmethod
+    def status(self):
+        pass
